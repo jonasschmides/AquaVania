924bcd5ead46b7ceaa51429c63d615c57e2e9b5e
Jonas Schmidbauer <jonas.schmidbauer@technikum-wien.at>
2018-01-11 19:30:12 +0100

    added Level elements and improved camera controller

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -7,14 +7,18 @@ public class CameraController : MonoBehaviour {
 	public GameObject player;
 
 	private Vector3 offset;
+	private float offsetZ;
 
 	// Use this for initialization
 	void Start () {
-		offset = transform.position + player.transform.position;
+		offset = transform.position - player.transform.position;
+		offsetZ = - offset[1];
 	}
 	
 	// Update is called once per frame
 	void LateUpdate () {
-		transform.position = player.transform.position;
+		Vector3 position = player.transform.position + offset;
+		position [1] = offsetZ;
+		transform.position = position;
 	}
 }
