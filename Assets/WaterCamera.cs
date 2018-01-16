109ce4b546872f8fe626c8cbdd2b02a1855e3113
Jonas Schmidbauer <jonas.schmidbauer@technikum-wien.at>
2018-01-16 18:46:19 +0100

    worked on level L2-1

--- Assets/WaterCamera.cs
+++ Assets/WaterCamera.cs
@@ -7,6 +7,11 @@ public class WaterCamera : MonoBehaviour {
 
 	public Material material;
 
+	void Start()
+	{
+		GetComponent<Camera>().cullingMask = 1 << 0;
+	}
+
 	void OnRenderImage(RenderTexture source, RenderTexture destination)
 	{
 		Graphics.Blit(source, destination, material);
